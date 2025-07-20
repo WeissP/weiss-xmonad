87c9ae52e56c3c552b82a749bf128ea2d9123ba7
weiss <kingbaiing@163.com>
2025-07-20 16:40:50 +0200

    add develop shell for xmonad-contrib

---
+++ vendor/xmonad-contrib/develop.nix
@@ -0,0 +1,12 @@
+pkgs: devInputs:
+devInputs
+// {
+  nativeBuildInputs = with pkgs.haskellPackages; [
+    cabal-install
+    hlint
+    ghcid
+    ormolu
+    implicit-hie
+    haskell-language-server
+  ];
+}
